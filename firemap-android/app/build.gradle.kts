64a4be25ce1828fabdfab891da901e61f13fb2b2
hj8kggdtv9-star <hj8kggdtv9@privaterelay.appleid.com>
2026-09-02 10:25:02 +0200

    Prepare FIREMAP Android v1 build

--- firemap-android/app/build.gradle.kts
+++ firemap-android/app/build.gradle.kts
@@ -3,7 +3,8 @@ plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
 android {
     namespace = "pl.firemap.zastep"
     compileSdk = 35
-    defaultConfig { applicationId = "pl.firemap.zastep"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "0.1.0" }
+    defaultConfig { applicationId = "pl.firemap.zastep"; minSdk = 26; targetSdk = 35; versionCode = 2; versionName = "1.0.0-beta1" }
+    buildTypes { release { isMinifyEnabled = false } }
     buildFeatures { viewBinding = false }
 }
 
